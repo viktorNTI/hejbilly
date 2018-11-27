7dd83aaa656f4ac4632073fd69c0d0cdaec8ab73
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-11-27 09:21:10 +0100

    fifth commit

---
+++ main.py
@@ -0,0 +1,15 @@
+choice = 0
+message = " "
+meny = 0
+
+while meny !=3:
+    try: 
+        meny = int(input("Läs loggar (1) Skriv ut en logg (2) Avsluta (3)")) 
+    except:
+        print("Välj (1), (2) eller (3)")
+    if meny == 1:
+        f = open('log.txt', 'r')
+        for line in f:
+            print(line)
+        f.close()
+
