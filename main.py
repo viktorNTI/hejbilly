734aeb94914aae89f7f333840812741f62a40ada
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-11-27 10:24:19 +0100

    ninth commit

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-choice = 0
-message = " "
-meny = 0
+choice = 0 #här bestämmer vi ett värde på variabeln så programmet inte kraschar
+message = " " #här sätter vi en tom sträng i variabeln så vi kan använda den senare
+meny = 0 #här bestämmer vi ett värde på variabeln så programmet inte kraschar
 
-while meny !=3:
-    try: 
-        meny = int(input("Läs loggar (1) Skriv ut en logg (2) Avsluta (3)")) 
-    except:
-        print("Välj (1), (2) eller (3)")
-    if meny == 1:
-        f = open('log.txt', 'r')
-        for line in f:
-            print(line)
-        f.close()
+while meny !=3: #medans variabeln inte är 3 så loopar den
+    try: #så försöker den...
+        meny = int(input("Läs loggar (1) Skriv ut en logg (2) Avsluta (3)")) #få en input från användaren 
+    except: #om användaren inte anger 1, 2 eller 3 så skriver den ut...
+        print("Välj (1), (2) eller (3)") #ett meddelande
+    if meny == 1: #om användaren anger 1
+        f = open('log.txt', 'r') #så öppnar programmet log.txt med läsrättigheter
+        for line in f: #en loop som skriver ut det som står i log.txt
+            print(line) #den skriver ut linjen
+        f.close() #programmet stänger log.txt och sparar innehållet
 
-    elif meny == 2:
-        f = open('log.txt', 'w+')
-        message = (input("Skriv till loggen"))
-        f.write(message)     
-        f.close() 
+    elif meny == 2: #om användaren anger 2
+        f = open('log.txt', 'w+') #så öppnar programmet log.txt med write and read funktioner
+        message = (input("Skriv till loggen")) #programmet frågar efter en input i syfte att skriva in i log.txt som en sträng
+        f.write(message) #skriver in vad användaren tilldelade variabeln message i log.txt     
+        f.close() #programmet stänger log.txt och sparar innehållet
